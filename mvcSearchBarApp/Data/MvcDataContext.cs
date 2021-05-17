c534450c2da1cb4d2315b5df1b90f3d94c622851
sakeel jawfer <sakeeljawfer@gmail.com>
2021-05-17 12:41:41 +0530

    finel commit

---
+++ mvcSearchBarApp/Data/MvcDataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using mvcSearchBarApp.Models;
+
+namespace mvcSearchBarApp.Data
+{
+    public class MvcDataContext : DbContext
+    {
+        public MvcDataContext(DbContextOptions<MvcDataContext> options)
+             : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
